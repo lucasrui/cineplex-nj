49a229e7ab8339268f2ef8bb02dde3741896567c
tuilu <ljfwind@gmail.com>
2015-10-23 00:37:54 +0800

    first build

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var path  = require('path');
 var bodyParser  = require('body-parser');
+var mongoose = require('mongoose');
+var _ = require('underscore');
+var Movie = require('./models/movie');
 var port  = process.env.PORT || 3000;
 var app = express();
 
+mongoose.connect('mongodb://localhost/cineplex');
+
 app.set('views','./views/pages');
 app.set('view engine','jade');
+app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(bodyParser());
-app.use(bodyParser.json()); // for parsing application/json
 
 app.use(express.static(path.join(__dirname, 'bower_components')))
 
@@ -18,7 +23,17 @@ console.log('cineplex started on prot ' + port);
 
 //index page
 app.get('/', function(req, res) {
-	res.render('index', {
+	Movie.fetch(function(err,movies) {
+		if(err) {
+			console.log(err);
+		}
+		res.render('index', {
+			title: 'cineplex 首页',
+			movies: movies
+		})
+	})
+
+	/*res.render('index', {
 		title: 'cineplex 首页',
 		movies: [{
 		title: '机械战警',
@@ -33,19 +48,65 @@ app.get('/', function(req, res) {
 		_id: 3,
 		poster: 'http://img.mukewang.com/533e4ce900010ae802000200-100-100.jpg'
 	}]
-})
+})*/
 })
 
 //detail page
 app.get('/movie/:id', function(req, res) {
-	res.render('detail', {
-		title: 'cineplex 详情',
-		movie: {
-			doctor: '啊啊啊',
-			country: '美国',
-			title: '机械战警'
-		}
+	var id = req.params.id;
+	Movie.findById(id, function(err, movie) {
+		res.render('detail', {
+			title: 'cineplex 详情' + movie.title,
+			movie: movie
+		})
 	})
+})
+//admin update
+app.get('/admin/update/:id', function(req, res) {
+	var id = req.params.id;
+	if(id) {
+		Movie.findById(id, function(err, movie) {
+			res.render('admin', {
+				title: 'cineplex 后台',
+				movie: movie
+			})
+		})
+	}
+})
+//admin post movie
+app.post('/admin/movie/new', function(res, req) {
+	console.log(res.body);
+	var movieObj = req.body.movie;
+	if(!movieObj)console.log(req);
+	var id = req.body.movie._id;
+	var _movie
+	if(id !== 'undefined') {
+		Movie.findById(id, function(err, movie) {
+			if(err) {
+				console.log(err);
+			}
+			_movie = _.extend(movie, movieObj);
+			_movie.save(function(err, movie) {
+				if(err) {
+					console.log(err);
+				}
+				res.redirect('/movie/' + movie._id);
+			})
+		})
+	}
+	else {
+		_movie = new Movie({
+			doctor: movieObj.doctor,
+			title: movieObj.title
+		})
+
+		_movie.save(function(err, movie) {
+			if(err) {
+				console.log(err);				
+			}
+			res.redirect('/movie/' + movie._id);
+		})
+	}
 })
 
 //admin page
@@ -53,26 +114,27 @@ app.get('/admin/movie', function(req, res) {
 	res.render('admin', {
 		title: 'cineplex 后台',
 		movie: {
-			title: '',
-			doctor: '',
+			title: 'aa',
+			doctor: ''/*,
 			country: '',
 			year: '',
 			poster: '',
 			flash: '',
 			summary: '',
-			language: ''
+			language: ''*/
 		}
 	})
 })
 
 //list page
 app.get('/admin/list', function(req, res) {
-	res.render('list', {
-		title: 'cineplex 列表',
-		movies: [{
-			doctor: '啊啊啊',
-			country: '美国',
-			title: '机械战警'
-		}]
+	Movie.fetch(function(err,movies) {
+		if(err) {
+			console.log(err);
+		}
+		res.render('lsit', {
+			title: 'cineplex 列表页',
+			movies: movies
+		})
 	})
 })
